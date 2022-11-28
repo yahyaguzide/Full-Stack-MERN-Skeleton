92b75a783c458650ff51f675c3af72bd2c07a8f9
Yahya Güzide <yahya.guezide@sva.de>
2022-11-28 15:35:40 +0100

    filled user.controler with create

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,18 +1,37 @@
 import User from '../models/user.model'
 import extend from 'lodash/extend'
 import errorHandler from './error.controller'
+import { json } from 'body-parser'
+import { validate } from 'schema-utils'
 
 /* NOTE: page: 172 Full-Stack React Projecs, Second Edition
 lodash is a JavaScript library that provides utility functions
 for common programming tasks, including the manipulation of arrays and objects.
 */
 
-const create = (req, res, next) => {
-    // TODO:
+const create = async (req, res) => {
+    const user = new User(req.body)
+    try{
+        await user.save()
+        return res.status(200).json({
+            message: "Successfully signed up!"
+        })
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
 }
 
-const list = (req, res) => {
-    // TODO:
+const list = async (req, res) => {
+    try {
+        let users = await User.find().select('name email update created')
+        res.json(users)
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
 }
 
 const userById = (req, res, nex, id) => {
